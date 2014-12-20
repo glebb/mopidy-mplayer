d3ababc4693a179cfedc5462a717b6b5bd784d19
Antti Niiles <antti.niiles@gmail.com>
2014-12-20 19:20:06 +0200

    provide just playlist

--- mopidy_mplayer/backend.py
+++ mopidy_mplayer/backend.py
@@ -12,21 +12,12 @@ logger = logging.getLogger(__name__)
 class MplayerBackend(pykka.ThreadingActor, backend.Backend):
     def __init__(self, config, audio):
         super(MplayerBackend, self).__init__()
-        from .audio import MplayerAudio
-        from .library import MplayerLibraryProvider
+        #from .audio import MplayerAudio
+        #from .library import MplayerLibraryProvider
         from .playlists import MplayerPlaylistsProvider
-        self.audio = MplayerAudio(config)
+        #self.audio = MplayerAudio(config)
         self.uri_schemes = ['mplayer']
-        self.playback = MediaplayerPlaybackProvider(audio=self.audio,
-                                                    backend=self)
-        self.library = MplayerLibraryProvider(backend=self)
+        #self.playback = MediaplayerPlaybackProvider(audio=self.audio,
+        #                                            backend=self)
+        #self.library = MplayerLibraryProvider(backend=self)
         self.playlists = MplayerPlaylistsProvider(backend=self, config=config)
-
-    def on_stop(self):
-        logger.debug('Closing Mplayer')
-        self.audio.player.stop()
-        self.audio.player.quit()
-
-
-class MediaplayerPlaybackProvider(backend.PlaybackProvider):
-    pass
